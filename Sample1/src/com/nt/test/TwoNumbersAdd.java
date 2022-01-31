d0bf5e740b3eee9528d857923cb84721b36c3e97
TL <TL@gmail.com>
2022-01-31 12:36:58 +0530

    Dev1 Modified Code

--- Sample1/src/com/nt/test/TwoNumbersAdd.java
+++ Sample1/src/com/nt/test/TwoNumbersAdd.java
@@ -1,14 +1,13 @@
 package com.nt.test;
 
-import com.nt.beans.Addition;
-
 public class TwoNumbersAdd {
-	 int a=20;
-	 int b=30;
-	 int c=a+b;
+	 static int a=20;
+	 static int b=30;
+	static int  result =a+b;
+	 
 		public static void main(String args[]) {
-			Addition ad = new Addition();
-			System.out.println(ad.c);
+			//Addition ad = new Addition();
+			System.out.println(result);
 			
 		}
 
