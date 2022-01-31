6c7f4c0feb4635e522bf0087b6e10fd605d41f79
TL <TL@gmail.com>
2022-01-31 12:21:54 +0530

    Addition Of Two Numbers

---
+++ Sample1/src/com/nt/test/TwoNumbersAdd.java
@@ -0,0 +1,15 @@
+package com.nt.test;
+
+import com.nt.beans.Addition;
+
+public class TwoNumbersAdd {
+	 int a=20;
+	 int b=30;
+	 int c=a+b;
+		public static void main(String args[]) {
+			Addition ad = new Addition();
+			System.out.println(ad.c);
+			
+		}
+
+}
