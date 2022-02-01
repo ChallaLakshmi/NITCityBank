3a0304ba21049e94f6569fef90f6d7c0c90be47a
TL <TL@gmail.com>
2022-02-01 12:53:05 +0530

    Addition.java modified by Dev1

--- Sample1/src/com/nt/beans/Addition.java
+++ Sample1/src/com/nt/beans/Addition.java
@@ -3,10 +3,11 @@ package com.nt.beans;
 public class Addition {
     int a=20;
 	 int b=30;
-	 public int c=a+b;
+	 int c=40;
+	 public int x=a+b*c;
 		public static void main(String args[]) {
 			Addition ad = new Addition();
-			System.out.println(ad.c);
+			System.out.println(ad.x);
 			
 		}
 		}
